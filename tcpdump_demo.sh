017b02984733236a6ac65bbe2f2df2d87851c0f5
mannd00 <35670627+mannd00@users.noreply.github.com>
2018-04-03 13:40:11 -0500

    Update tcpdump_demo.sh

--- tcpdump_demo.sh
+++ tcpdump_demo.sh
@@ -2,5 +2,18 @@
 
 echo "Welcome to the TCPDump Demo!"
 
-echo " ""
+echo "We will be taking a look at TCPDump and seeing wha tit can do."
+
+echo -e "\nLets run it using -i any to just get a quick look at any interface that might be transmitting."
+tcpdump -i any
+
+echo -e "\nTCPDump offers a veriety of command line options to specify what type of outpu you are looking for. We will try -ttttnnvvS which uses human readable time-stamps, does not resove host names OR port numbers, in very verbose mode, and shows absolute sequence numbers."  
+wait 3
+
+tcpdump -ttttnnvvS
+
+echo -e "\nMaybe we want to see all what is going on with a certain host, such as the local router which is usually at 192.168.1.1 (can change this based on your own)."
+wait 3
+tcpdump host 192.168.1.1
+
 
