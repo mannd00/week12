f452bab3d340bcd66726450c04747c65f9f1a79b
mannd00 <35670627+mannd00@users.noreply.github.com>
2018-04-03 20:31:19 -0500

    Update Mann_Ch11.sh

--- Mann_Ch11.sh
+++ Mann_Ch11.sh
@@ -2,5 +2,15 @@
 
 echo "Welcome to Chapter 11!"
 
-echo " ""
+echo -e "The Bash shell interprets commands with \' and \" differently."
+
+echo $100
+echo "$100"
+echo '$100'
+
+echo -e "\n See how the last three lines are different? All in the syntax"
+
+sleep 3
+
+echo -e "\n Use a \' around lines that should be seen as a single chunk of parameter. Use \" and the shell will expand variables in it"
 
